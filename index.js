aa107838d669607a16e82e337802119a4319ff8d
DhanaNugraha <dhananugraha1511@gmail.com>
2024-11-19 12:41:51 +0700

    feat: facility

--- index.js
+++ index.js
@@ -9,4 +9,45 @@ function changePage(shift) {
     // console.log(heroPage);
   }
 
+// animation for facilities carousel
 
+let curPos = 0
+
+function facility(direction) {
+  if (direction == 'right') {
+    if (curPos == 4) {
+      curPos = 0
+      let newPage = 0
+      console.log(curPos);
+      document.getElementById("facilityCarouselContainer").style.transform ="translateX(" + newPage + "%)";
+      console.log(curPos);
+    }
+    else {
+      let newPage = (-curPos * 100) - 100
+      console.log(curPos);
+      document.getElementById("facilityCarouselContainer").style.transform ="translateX(" + newPage + "%)";
+      console.log(curPos);
+      curPos += 1
+    }
+    
+  }
+
+  else if (direction == 'left') {
+    if (curPos == 0) {
+      curPos = 4
+      let newPage = -400
+      console.log(curPos);
+      document.getElementById("facilityCarouselContainer").style.transform ="translateX(" + newPage + "%)";
+      console.log(curPos);
+    }
+    else {
+      let newPage = (-curPos * 100) + 100
+      console.log(curPos);
+      document.getElementById("facilityCarouselContainer").style.transform ="translateX(" + newPage + "%)";
+      console.log(curPos);
+      curPos -= 1
+    }
+  }
+
+  
+}
